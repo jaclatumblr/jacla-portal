48ef7926f7d9315addb417d0725bd730bd38bbaa
yanai kotaro <74409814+yanai00@users.noreply.github.com>
2025-12-18 01:17:13 +0900

    add page scaffolds

---
+++ app/pa/page.tsx
@@ -0,0 +1,8 @@
+﻿export default function Page() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold">app\pa\page.tsx</h1>
+      <p className="mt-2 text-sm text-gray-500">TODO</p>
+    </div>
+  );
+}
